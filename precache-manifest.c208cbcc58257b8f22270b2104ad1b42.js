96a3b3bce5b28043981d779f6a95062cbf1ce1a9
RodrigoNeoEx <draja.acc@gmail.com>
2021-06-07 10:52:53 -0300

    Updates

---
+++ precache-manifest.c208cbcc58257b8f22270b2104ad1b42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "309d9fa2ad3662e382913267383bdab2",
+    "url": "/Movie-Cards-Library/index.html"
+  },
+  {
+    "revision": "b2c32143127d9936656a",
+    "url": "/Movie-Cards-Library/static/css/main.51b46e73.chunk.css"
+  },
+  {
+    "revision": "6bed614db731c49c7fb2",
+    "url": "/Movie-Cards-Library/static/js/2.77f2544d.chunk.js"
+  },
+  {
+    "revision": "b2c32143127d9936656a",
+    "url": "/Movie-Cards-Library/static/js/main.b0cebea6.chunk.js"
+  },
+  {
+    "revision": "2a3b780a971c4a4ba490",
+    "url": "/Movie-Cards-Library/static/js/runtime-main.ab563e64.js"
+  }
+]);
